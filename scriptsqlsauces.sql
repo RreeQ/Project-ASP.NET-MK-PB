2ad0d91710302ae830b7bbece93afb6828b44d8c
RreeQ <marcinek877@gmail.com>
2018-12-11 23:35:19 +0100

    Dodaje sosy do sql
    W .sql są sosy... potem trzeba je dodac :)

--- scriptsqlsauces.sql
+++ scriptsqlsauces.sql
@@ -1,7 +1,12 @@
+using BestHotDogs;
+
 SET IDENTITY_INSERT [dbo].[Sauces] ON
-INSERT INTO [dbo].[Sauces] ([Id], [Name]) VALUES (1, N'Meksykañski')
-INSERT INTO [dbo].[Sauces] ([Id], [Name]) VALUES (2, N'Duñski')
-INSERT INTO [dbo].[Sauces] ([Id], [Name]) VALUES (3, N'Musztarda')
-INSERT INTO [dbo].[Sauces] ([Id], [Name]) VALUES (4, N'Keczup')
-INSERT INTO [dbo].[Sauces] ([Id], [Name]) VALUES (5, N'Majonez')
+INSERT INTO [dbo].[Sauces] ([Id], [Name]) VALUES (1, N'Majonez')
+INSERT INTO [dbo].[Sauces] ([Id], [Name]) VALUES (2, N'Keczup')
+INSERT INTO [dbo].[Sauces] ([Id], [Name]) VALUES (3, N'Duñski')
+INSERT INTO [dbo].[Sauces] ([Id], [Name]) VALUES (4, N'Musztarda')
+INSERT INTO [dbo].[Sauces] ([Id], [Name]) VALUES (5, N'Tysi¹ca wysp')
+INSERT INTO [dbo].[Sauces] ([Id], [Name]) VALUES (6, N'Diabolo')
+INSERT INTO [dbo].[Sauces] ([Id], [Name]) VALUES (7, N'Arabski £agodny')
+INSERT INTO [dbo].[Sauces] ([Id], [Name]) VALUES (8, N'Arabski Ostry')
 SET IDENTITY_INSERT [dbo].[Sauces] OFF
